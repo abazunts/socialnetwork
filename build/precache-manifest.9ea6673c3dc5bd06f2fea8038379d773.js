b1c652c157c37e40a60b7482d9e306ae5693e110
Artem Bazunts <bazunc@gmail.com>
2019-07-22 17:00:38 +0500

    build

---
+++ build/precache-manifest.9ea6673c3dc5bd06f2fea8038379d773.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "314dc0ba80e407e0040b",
+    "url": "/static/css/main.3d044956.chunk.css"
+  },
+  {
+    "revision": "314dc0ba80e407e0040b",
+    "url": "/static/js/main.314dc0ba.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1a8119bd39b1f10d1bb9",
+    "url": "/static/js/2.1a8119bd.chunk.js"
+  },
+  {
+    "revision": "78df590d7b31d226833df0ebad45a5d2",
+    "url": "/static/media/Photobigprofile.78df590d.png"
+  },
+  {
+    "revision": "1e269a9f4f4c1e7b93d1da8e02f9ad35",
+    "url": "/static/media/myphoto.1e269a9f.png"
+  },
+  {
+    "revision": "7c5f2d7386bb61b1d0f705a8b0c8dde3",
+    "url": "/static/media/ajax-loader.7c5f2d73.gif"
+  },
+  {
+    "revision": "f30f6e50dd0f3348b748316c12eafc16",
+    "url": "/static/media/photo.f30f6e50.png"
+  },
+  {
+    "revision": "8db0f8dff85acf985c56bd3fd662c7a5",
+    "url": "/static/media/rfriendsavatar.8db0f8df.png"
+  },
+  {
+    "revision": "824a1adf0e8e9a426665b45ee804f3e5",
+    "url": "/static/media/rfriendsphoto.824a1adf.png"
+  },
+  {
+    "revision": "e2ea906bcf8e615abdf002ed8fbd1dda",
+    "url": "/static/media/userphoto.e2ea906b.png"
+  },
+  {
+    "revision": "dc9092c6ae76205a82e26bfcd43e5b6a",
+    "url": "/index.html"
+  }
+];
