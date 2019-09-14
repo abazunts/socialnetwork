83108996b425b4384e877a06153e6f50b660f6c3
abazunts home <bazunc@gmail.com>
2019-09-14 23:31:29 +0500

    Updates

---
+++ precache-manifest.9e19a9056af3e238ade7159e78713960.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3d071c52662c6c8a876",
+    "url": "/socialnetwork/static/css/main.b1bc54b8.chunk.css"
+  },
+  {
+    "revision": "c3d071c52662c6c8a876",
+    "url": "/socialnetwork/static/js/main.c3d071c5.chunk.js"
+  },
+  {
+    "revision": "7e06743d830a71b5c19c",
+    "url": "/socialnetwork/static/js/runtime~main.7e06743d.js"
+  },
+  {
+    "revision": "1a8119bd39b1f10d1bb9",
+    "url": "/socialnetwork/static/js/2.1a8119bd.chunk.js"
+  },
+  {
+    "revision": "78df590d7b31d226833df0ebad45a5d2",
+    "url": "/socialnetwork/static/media/Photobigprofile.78df590d.png"
+  },
+  {
+    "revision": "1e269a9f4f4c1e7b93d1da8e02f9ad35",
+    "url": "/socialnetwork/static/media/myphoto.1e269a9f.png"
+  },
+  {
+    "revision": "7c5f2d7386bb61b1d0f705a8b0c8dde3",
+    "url": "/socialnetwork/static/media/ajax-loader.7c5f2d73.gif"
+  },
+  {
+    "revision": "f30f6e50dd0f3348b748316c12eafc16",
+    "url": "/socialnetwork/static/media/photo.f30f6e50.png"
+  },
+  {
+    "revision": "8db0f8dff85acf985c56bd3fd662c7a5",
+    "url": "/socialnetwork/static/media/rfriendsavatar.8db0f8df.png"
+  },
+  {
+    "revision": "824a1adf0e8e9a426665b45ee804f3e5",
+    "url": "/socialnetwork/static/media/rfriendsphoto.824a1adf.png"
+  },
+  {
+    "revision": "e2ea906bcf8e615abdf002ed8fbd1dda",
+    "url": "/socialnetwork/static/media/userphoto.e2ea906b.png"
+  },
+  {
+    "revision": "222f5b6fd7b849f4b1fe7379fd136dfd",
+    "url": "/socialnetwork/index.html"
+  }
+];
